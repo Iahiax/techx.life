1a7f7deeb49c54e72db79195db4d3c4caa4f814c
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-03-04 06:26:20 +0300

    Update OrgUser.php

--- app/Models/OrgUser.php
+++ app/Models/OrgUser.php
@@ -1,1 +1,7 @@
-
+Schema::create('org_users', function (Blueprint $table) {
+    $table->id();
+    $table->foreignId('user_id')->constrained()->onDelete('cascade');
+    $table->foreignId('org_id')->constrained()->onDelete('cascade');
+    $table->string('role')->default('OrgUser'); // OrgAdmin, OrgUser
+    $table->timestamps();
+});
