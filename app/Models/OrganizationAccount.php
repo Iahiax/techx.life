4767beb7834c2294983e9827abbae2e2f027b3b7
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-03-04 06:27:36 +0300

    Update OrganizationAccount.php

--- app/Models/OrganizationAccount.php
+++ app/Models/OrganizationAccount.php
@@ -1,1 +1,9 @@
-
+Schema::create('organization_accounts', function (Blueprint $table) {
+    $table->id();
+    $table->foreignId('org_id')->constrained()->onDelete('cascade');
+    $table->string('iban');
+    $table->string('bank_name');
+    $table->string('account_name');
+    $table->boolean('is_default')->default(false);
+    $table->timestamps();
+});
