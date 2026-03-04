86e47031d71d7538cc0939bd4ae34fb1c9cb9128
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-03-04 06:41:45 +0300

    Update ReportController.php

--- app/Http/Controllers/Customer/ReportController.php
+++ app/Http/Controllers/Customer/ReportController.php
@@ -1,1 +1,8 @@
-
+// app/Http/Controllers/Customer/ReportController.php
+public function printContracts()
+{
+    $customer = auth()->user();
+    $contracts = Contract::where('customer_id', $customer->id)->get();
+    $pdf = Pdf::loadView('pdf.customer_contracts', compact('contracts'));
+    return $pdf->download('تقرير_العقود.pdf');
+}
