35eac91adfd6331c285187075ac9b895c4b23021
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-03-04 08:14:29 +0300

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,1 +1,25 @@
+<?php
 
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+// يمكن إضافة أوامر مخصصة هنا إذا كانت بسيطة ولا تتطلب كلاس منفصل
+// مثال:
+// Artisan::command('deductions:test', function () {
+//     $this->info('Testing deductions command...');
+// })->purpose('Test deduction command');
