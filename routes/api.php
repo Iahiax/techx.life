02fa5c4fbc7d21413629e15c44cea9525aecba3e
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-03-04 06:42:36 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,5 @@
-
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('contracts', Api\ContractController::class);
+    Route::get('deductions', [Api\DeductionController::class, 'index']);
+    Route::post('webhook/lean', [Api\WebhookController::class, 'handleLean']);
+});
