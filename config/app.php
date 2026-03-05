5b34557a180902a76397922d759f49bad4d937f7
Iahiax <108660221+Iahiax@users.noreply.github.com>
2026-03-05 20:39:43 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -1,14 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\ServiceProvider;
+
 return [
 
     /*
     |--------------------------------------------------------------------------
-    | اسم التطبيق
+    | Application Name
     |--------------------------------------------------------------------------
     |
-    | هذا القيمة هي اسم تطبيقك. تُستخدم عندما يحتاج الإطار إلى وضع اسم التطبيق
-    | في إشعار أو أي موقع آخر يتطلبه التطبيق أو حزمه.
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
     |
     */
 
@@ -16,12 +20,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | بيئة التطبيق
+    | Application Environment
     |--------------------------------------------------------------------------
     |
-    | هذه القيمة تحدد "البيئة" التي يعمل فيها تطبيقك حاليًا.
-    | قد تحدد كيفية تفضيل تكوين الخدمات المختلفة التي يستخدمها التطبيق.
-    | قم بتعيين هذا في ملف ".env" الخاص بك.
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services the application utilizes. Set this in your ".env" file.
     |
     */
 
@@ -29,25 +33,25 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | وضع تصحيح الأخطاء
+    | Application Debug Mode
     |--------------------------------------------------------------------------
     |
-    | عندما يكون تطبيقك في وضع التصحيح، سيتم عرض رسائل خطأ مفصلة مع
-    | تتبع الأخطاء على كل خطأ يحدث داخل تطبيقك. إذا تم تعطيله،
-    | سيتم عرض صفحة خطأ عامة بسيطة.
+    | When your application is in debug mode, detailed error messages with
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
-    | رابط التطبيق
+    | Application URL
     |--------------------------------------------------------------------------
     |
-    | يستخدم هذا الرابط بواسطة وحدة التحكم لتوليد روابط بشكل صحيح عند استخدام
-    | أداة سطر الأوامر Artisan. يجب عليك تعيين هذا إلى الجذر
-    | لتطبيقك بحيث يتم استخدامه عند تشغيل مهام Artisan.
+    | This URL is used by the console to properly generate URLs when using
+    | the Artisan command line tool. You should set this to the root of
+    | your application so that it is used when running Artisan tasks.
     |
     */
 
@@ -57,11 +61,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | المنطقة الزمنية للتطبيق
+    | Application Timezone
     |--------------------------------------------------------------------------
     |
-    | هنا يمكنك تحديد المنطقة الزمنية الافتراضية لتطبيقك، والتي ستستخدمها
-    | دوال PHP للتاريخ والوقت. قمنا بتعيين قيمة معقولة افتراضية لك.
+    | Here you may specify the default timezone for your application, which
+    | will be used by the PHP date and date-time functions. We have gone
+    | ahead and set this to a sensible default for you out of the box.
     |
     */
 
@@ -69,12 +74,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | إعدادات اللغة المحلية للتطبيق
+    | Application Locale Configuration
     |--------------------------------------------------------------------------
     |
-    | تحدد اللغة المحلية للتطبيق اللغة الافتراضية التي ستستخدمها
-    | مزود خدمة الترجمة. أنت حر في تعيين هذه القيمة
-    | إلى أي من اللغات المدعومة من التطبيق.
+    | The application locale determines the default locale that will be used
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
     |
     */
 
@@ -82,12 +87,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | اللغة المحلية البديلة للتطبيق
+    | Application Fallback Locale
     |--------------------------------------------------------------------------
     |
-    | اللغة البديلة تحدد اللغة التي سيتم استخدامها عندما تكون اللغة الحالية
-    | غير متوفرة. يمكنك تغيير القيمة لتتوافق مع أي من مجلدات اللغة
-    | المتوفرة من خلال تطبيقك.
+    | The fallback locale determines the locale to use when the current one
+    | is not available. You may change the value to correspond to any of
+    | the language folders that are provided through your application.
     |
     */
 
@@ -95,12 +100,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | لغة Faker
+    | Faker Locale
     |--------------------------------------------------------------------------
     |
-    | سيتم استخدام هذه اللغة بواسطة مكتبة Faker PHP عند توليد بيانات
-    | وهمية لقاعدة البيانات. على سبيل المثال، سيتم استخدام هذا للحصول على
-    | أرقام هواتف محلية، معلومات عناوين الشوارع والمزيد.
+    | This locale will be used by the Faker PHP library when generating fake
+    | data for your database seeds. For example, this will be used to get
+    | localized telephone numbers, street address information and more.
     |
     */
 
@@ -108,12 +113,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | مفتاح التشفير
+    | Encryption Key
     |--------------------------------------------------------------------------
     |
-    | يستخدم هذا المفتاح بواسطة خدمات التشفير في Laravel ويجب تعيينه
-    | إلى سلسلة عشوائية طويلة من 32 حرفًا لضمان أن جميع القيم المشفرة
-    | آمنة. يجب عليك فعل ذلك قبل نشر التطبيق.
+    | This key is utilized by Laravel's encryption services and should be set
+    | to a random, 32 character string to ensure that all encrypted values
+    | are secure. You should do this prior to deploying the application.
     |
     */
 
@@ -129,14 +134,14 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | وضع الصيانة
+    | Maintenance Mode Driver
     |--------------------------------------------------------------------------
     |
-    | تحدد خيارات التكوين هذه السائق المستخدم لتحديد وإدارة
-    | حالة "وضع الصيانة" في Laravel. سائق "cache" سيسمح
-    | بالتحكم في وضع الصيانة عبر عدة عمليات.
+    | These configuration options determine the driver used to determine and
+    | manage Laravel's "maintenance mode" status. The "cache" driver will
+    | allow maintenance mode to be controlled across multiple machines.
     |
-    | السائقون المدعومون: "file", "cache"
+    | Supported drivers: "file", "cache"
     |
     */
 
@@ -147,79 +152,55 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | مزودي الخدمة المحملة تلقائياً
+    | Autoloaded Service Providers
     |--------------------------------------------------------------------------
     |
-    | مزودو الخدمة المدرجون هنا سيتم تحميلهم تلقائياً عند
-    | طلب تطبيقك. لا تتردد في إضافة خدماتك الخاصة
-    | إلى هذه المصفوفة لتوسيع الوظائف.
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
     |
     */
 
-    'providers' => [
+    'providers' => ServiceProvider::defaultProviders()->merge([
         /*
-         * مزودو خدمات إطار Laravel...
-         */
-        Illuminate\Auth\AuthServiceProvider::class,
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        Illuminate\Bus\BusServiceProvider::class,
-        Illuminate\Cache\CacheServiceProvider::class,
-        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Cookie\CookieServiceProvider::class,
-        Illuminate\Database\DatabaseServiceProvider::class,
-        Illuminate\Encryption\EncryptionServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
-        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-        Illuminate\Hashing\HashServiceProvider::class,
-        Illuminate\Mail\MailServiceProvider::class,
-        Illuminate\Notifications\NotificationServiceProvider::class,
-        Illuminate\Pagination\PaginationServiceProvider::class,
-        Illuminate\Pipeline\PipelineServiceProvider::class,
-        Illuminate\Queue\QueueServiceProvider::class,
-        Illuminate\Redis\RedisServiceProvider::class,
-        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
-        Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
-        Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
-
-        /*
-         * مزودو خدمات الحزم...
+         * Package Service Providers...
          */
         Barryvdh\Dompdf\ServiceProvider::class,
 
         /*
-         * مزودو خدمات التطبيق...
+         * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
+        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-    ],
+    ])->toArray(),
 
     /*
     |--------------------------------------------------------------------------
-    | أسماء مستعارة للكلاسات
+    | Class Aliases
     |--------------------------------------------------------------------------
     |
-    | مصفوفة الأسماء المستعارة للكلاسات هذه ستُسجل عند بدء التطبيق.
-    | ومع ذلك، لا تتردد في تسجيل أي عدد تريده من الأسماء المستعارة لأن
-    | الأسماء المستعارة يتم تحميلها "كسولة" لذا لا تضر بالأداء.
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
     |
     */
 
-    'aliases' => [
-        'App' => Illuminate\Support\Facades\App::class,
-        'Arr' => Illuminate\Support\Arr::class,
-        'Artisan' => Illuminate\Support\Facades\Artisan::class,
-        'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
-        'Bus' => Illuminate\Support\Facades\Bus::class,
-        'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Config' => Illuminate\Support\Facades\Config::class,
-        'Cookie' => Illuminate\Support\Facades\Cookie::class,
-        'Crypt' => Illuminate\Support\Facades\Crypt::class,
+    'aliases' => Facade::defaultAliases()->merge([
+        // 'Example' => App\Facades\Example::class,
+        'PDF' => Barryvdh\Dompdf\Facade\Pdf::class,
+    ])->toArray(),
+
+];
+
+// إخفاء التحذيرات في بيئة الإنتاج
+if (env('APP_ENV') === 'production') {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+    error_reporting(0);
+}
         'Date' => Illuminate\Support\Facades\Date::class,
         'DB' => Illuminate\Support\Facades\DB::class,
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
